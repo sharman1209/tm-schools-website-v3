c27b2131334130a922b6638b286dccaf4a6cb756
sharman1209 <74037705+sharman1209@users.noreply.github.com>
2024-01-22 03:00:22 +0000

    Added php

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
   jobs = load_jobs_from_db()
-  return render_template('home.html', 
+  return render_template('home.php', 
                          jobs=jobs)
 
 @app.route("/api/jobs")
